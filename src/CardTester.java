90f90a7c6c16ff48aa722c8ea72f0e35ecb855a6
sadiahossain1 <sadiah16@nycstudents.net>
2024-02-29 12:29:02 -0500

    working

--- src/CardTester.java
+++ src/CardTester.java
@@ -1,2 +1,18 @@
-package PACKAGE_NAME;public class CardTester {
+/**
+ * This is a class that tests the Card class.
+ */
+public class CardTester {
+
+    /**
+     * The main method in this class checks the Card operations for consistency.
+     *	@param args is not used.
+     */
+    public static void main(String[] args) {
+        Card card1 = new Card("Diamond", "idk", 2);
+        Card card2 = new Card("Diamond", "idk", 2);
+        Card card3 = new Card("Hearts", "idk", 6);
+        card1.rank();
+        card2.suit();
+        card3.pointValue();
+    }
 }
